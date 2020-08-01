f9fbed09b29f9da277913bf59bf62bb62bf191b7
Rupank Bansal <rubansa@microsoft.com>
2020-08-01 17:02:05 +0530

    Added Communal Messages

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>Communal Harmony</h1>
+      <h1>Communal Harmony </h1>
       <h2>Happy Friendship Day!!</h2>
       <p>Cherish Our Memories!!</p>
     </div>
